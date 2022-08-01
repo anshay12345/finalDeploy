0aa357271ee81da906db1ba72ed6f5a993752899
anshay <anshay12345@gmail.com>
2022-08-01 12:36:23 +0530

    some changes

---
+++ src/components/PreviewArea.js
@@ -0,0 +1,10 @@
+import React from "react";
+import CatSprite from "./CatSprite";
+
+export default function PreviewArea() {
+  return (
+    <div className="flex-none h-full overflow-y p-8 ">
+      <CatSprite />
+    </div>
+  );
+}
